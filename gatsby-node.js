694f7c7d29c0f3d1ce6676bd6bb0d143d570f912
Gabriel V <gabriel.vial@irot.cl>
2019-02-28 00:03:50 -0300

    Test sitemap 2

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const { createFilePath } = require(`gatsby-source-filesystem`)
+import fs from 'fs-extra-promise'
+import sm from 'sitemap' 
 
 exports.onCreateWebpackConfig = ({
   stage,
@@ -59,4 +61,36 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
       resolve()
     })
   })
+}
+
+function pagesToSitemap(pages) {
+  const urls = pages.map((p) => {
+    if (p.path !== undefined) {
+      return {
+        url: p.path,
+        changefreq: 'daily',
+        priority: 0.7
+      }
+    }
+  })
+  // remove undefined (template pages)
+  return urls.filter(u => u !== undefined)
+}
+
+function generateSiteMap(pages) {
+  const sitemap = sm.createSitemap({
+    hostname: 'https://www.example.com',
+    cacheTime: '60000',
+    urls: pagesToSitemap(pages),
+  })
+  console.log('Generating sitemap.xml')
+  fs.writeFileSync(
+    `${__dirname}/public/sitemap.xml`,
+    sitemap.toString()
+  )
+}
+
+export function postBuild(pages, callback) {
+  generateSiteMap(pages)
+  callback()
 }
